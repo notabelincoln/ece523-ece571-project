3e86efa2982a9a354de9ba8813b03e8d94635b14
Abe <abejordandx@gmail.com>
2022-11-08 14:42:08 -0500

    Added headers to print columns

--- main.c
+++ main.c
@@ -13,6 +13,8 @@ int main(int argc, char **argv)
 {
 	double x;
 
+	printf("%5s,%10s,%10s\n","x","gcc","fixed");
+
 	for (x = -M_PI / 2; x <= M_PI / 2; x += 0.01)
 		printf("%5.2lf,%10.7lf,%10.7lf\n",
 				x,
