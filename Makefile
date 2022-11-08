5faba649aeeb3137a82110da619ae8cf3bd769d7
Abe <abejordandx@gmail.com>
2022-11-07 23:48:05 -0500

    Created basic test program for algorithms

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	gcc -o run-test main.c fixed-point-5-11.c
+	gcc -lm -o run-test main.c fixed-point-5-11.c
--- main.c
+++ main.c
@@ -5,9 +5,19 @@
  */
 
 #include <stdio.h>
+#include <math.h>
+
 #include "fixed-point-5-11.h"
 
 int main(int argc, char **argv)
 {
+	double x;
+
+	for (x = -M_PI / 2; x <= M_PI / 2; x += 0.01)
+		printf("%5.2lf,%10.7lf,%10.7lf\n",
+				x,
+				sin(x),
+				(double)sin_fixed(double_to_fixed(x)) / 2048);
+
 	return 0;
 }
