dffeb58bb647918617fede547e242e1e997ad03d
Abe <abejordandx@gmail.com>
2022-11-12 19:08:07 -0500

    Renamed functions, started function for integration of strictly ints

--- abe-math.c
+++ abe-math.c
@@ -4,9 +4,16 @@
  * abe-math.c
  */
 
-double a_integrate(double *x, double *y, unsigned long long int length)
+long int a_int(long int x, long int y, unsigned long int length)
 {
-	unsigned long long int i;
+	unsigned long int i;
+
+}
+
+/* integrate an array of doubles using rectangles */
+double a_lf_int_rect(double *x, double *y, unsigned long int length)
+{
+	unsigned long int i;
 	double ret;
 
 	if (length == 0)
@@ -14,15 +21,16 @@ double a_integrate(double *x, double *y, unsigned long long int length)
 
 	ret = 0;
 
-	for (i = 0; i < length; i++)
-		ret += x[i] * y[i];
+	for (i = 1; i < length; i++)
+		ret += (x[i] - x[i - 1]) * y[i - 1];
 
 	return ret;
 }
 
-double a_integrate_trapz(double *x, double *y, unsigned long long int length)
+/* integrate an array of doubles using trapezoids*/
+double a_lf_int_trap(double *x, double *y, unsigned long int length)
 {
-	unsigned long long int i;
+	unsigned long int i;
 	double ret;
 
 	if (length == 0)
