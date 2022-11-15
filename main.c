606f10b6ba35118ae8cefd7a64b527754c7daa31
Abe <abejordandx@gmail.com>
2022-11-14 23:59:05 -0500

    Swapped fixed-point numbers for double taylor series

--- main.c
+++ main.c
@@ -8,17 +8,16 @@
 #include <math.h>
 
 #include "fixed-point-5-11.h"
+#include "abe-math.h"
 
 int main(int argc, char **argv)
 {
 	double x;
-	double f_x_double;
 
-	printf("%5s,%10s,%10s\n","x","gcc","fixed");
+	printf("%6s,%15s,%15s\n", "x", "sin(x)", "sin_taylor(x)");
 
-	for (x = -M_PI / 2; x <= M_PI / 2; x += 0.01) {
-		f_x_double = (double)sin_fixed(double_to_fixed(x)) / 2048;
-		printf("%5.2lf,%10.7lf,%10.7lf\n", x, sin(x), f_x_double);
+	for (x = -(2 * M_PI); x <= (2 * M_PI); x += 0.0000001) {
+		printf("%6.3lf,%15.10lf,%15.10lf\n", x, sin(x), sin_taylor(x));
 	}
 
 	return 0;
