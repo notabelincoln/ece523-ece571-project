346230e73f42882547777053e78d682a7d0a9f67
Abe <abejordandx@gmail.com>
2022-11-14 23:47:20 -0500

    Added function declarations to abe-math.h

--- abe-math.h
+++ abe-math.h
@@ -4,7 +4,26 @@
  * abe-math.h
  */
 
-double a_integrate(double *x, double *y, unsigned long long int length);
+#ifndef ABE_MATH_H
+#define ABE_MATH_H
+/* integrate an array of long integers using rectangles */
+long int a_int_rect(long int *x, long int *y, unsigned long int length);
 
-double a_integrate_trapz(double *x, double *y, unsigned long long int length);
+/* integrate an array of long integers using trapezoids */
+long int a_int_trap(long int *x, long int *y, unsigned long int length);
 
+/* integrate an array of floats using rectangles */
+float a_fint_rect(float *x, float *y, unsigned long int length);
+
+/* integrate an array of floats using trapezoids*/
+float a_fint_trap(float *x, float *y, unsigned long int length);
+
+/* integrate an array of doubles using rectangles */
+double a_lfint_rect(double *x, double *y, unsigned long int length);
+
+/* integrate an array of doubles using trapezoids*/
+double a_lfint_trap(double *x, double *y, unsigned long int length);
+
+/* approximate a sin value using taylor series */
+double sin_taylor(double x);
+#endif
