fa6b15469f7e41fe55d65c7df9d13df3c3810676
Abe <abejordandx@gmail.com>
2022-11-21 19:45:07 -0500

    Added basic functionality to select functions

--- main.c
+++ main.c
@@ -6,19 +6,56 @@
 
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
-#include "fixed-point-5-11.h"
 #include "abe-math.h"
 
 int main(int argc, char **argv)
 {
+	int i;
+	int function;
+	int display;
+
 	double x;
+	double x_min;
+	double x_max;
+	double x_step;
+	double (*f)(double);
 
-	printf("%6s,%15s,%15s\n", "x", "sin(x)", "sin_taylor(x)");
-
-	for (x = -(2 * M_PI); x <= (2 * M_PI); x += 0.0000001) {
-		printf("%6.3lf,%15.10lf,%15.10lf\n", x, sin(x), sin_taylor(x));
+	/* check that user specifies function */
+	if ((argc < 2) || (argc > 3)) {
+		printf("USAGE: run-test [function] [display]\n");
+		return -1;
 	}
+
+	/* determine what function user wants to compute */
+	if (strcmp(argv[1], "sin") == 0) {
+		function = 0;
+		f = &sin_taylor;
+		x_min = -2 * M_PI;
+		x_max = 2 * M_PI;
+		x_step = 0.00000001;
+	} else {
+		function = 1;
+	}
+
+	/* determine whether to display data */
+	if (argc == 3) {
+		if (strcmp(argv[2], "display") == 0) {
+			display = 1;
+		} else {
+			display = 0;
+		}
+	}
+	if (display == 1) {
+		for (x = x_min; x <= x_max; x += x_step)
+			printf("%10.7lf, %20.17lf\n", x, (*f)(x));
+	} else {
+		for (x = x_min; x <= x_max; x += x_step)
+			(*f)(x);
+	}
+
+	printf("Completed\n");
 
 	return 0;
 }
