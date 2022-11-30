aeaf690835f17c1cb412cdbe5a1fa96ce53d80eb
Abe <abejordandx@gmail.com>
2022-11-30 18:13:35 -0500

    Added polynomial evaluation and other niceities to the float code

--- main-float.c
+++ main-float.c
@@ -18,6 +18,17 @@ int main(int argc, char **argv)
 
 	float x;
 
+	const float x_step = 0.000001;
+
+	const float x_sin_min = -4 * M_PI;
+	const float x_sin_max = 4 * M_PI;
+	
+	const float x_poly_min = -6.0;
+	const float x_poly_max = 6.0;
+
+	const float poly_coefs[] = {0.0, 0.0, 1.0};
+	const int poly_power = 2;
+
 	display = 0;
 
 	/* check if user called program only */
@@ -31,7 +42,11 @@ int main(int argc, char **argv)
 		if (strncmp(argv[i], "sin", 256) == 0) {
 			function = 0;
 			break;
+		} else if (strncmp(argv[i], "poly", 32) == 0) {
+			function = 1;
+			break;
 		}
+
 	}
 
 	/* determine whether to display the output */
@@ -39,23 +54,37 @@ int main(int argc, char **argv)
 		if (strncmp(argv[i], "display", 256) == 0) {
 			display = 1;
 			break;
-		}
+		}	
 	}
 
-	if (display == 0) {
+	if (display == 0) { /* run code without displaying results */
 		switch (function) {
 		case (0):
-			for (x = -(2 * M_PI); x <= (2 * M_PI); x += 0.000001)
+			for (x = x_sin_min; x <= x_sin_max; x += x_step)
 				sin_taylor(x);
 			break;
+		
+		case (1):
+			for (x = x_poly_min; x <= x_poly_max; x+= x_step)
+				poly_eval(poly_coefs, poly_power, x);
+			break;
 		}
-	} else {
+	} else { /* run code and display results */
 		switch (function) {
 		case (0):
 			printf("%11.7s,%10.7s\n","x","sin_taylor");
-			for (x = -(2 * M_PI); x <= (2 * M_PI); x += 0.000001)
+			for (x = x_sin_min; x <= x_sin_max; x += x_step)
 				printf("%11.7f,%10.7f\n",x,sin_taylor(x));
 			break;
+		case (1):
+			printf("%11s,%11s\n","x","f(x)");
+
+			for (x = x_poly_min; x <= x_poly_max; x+= x_step) {
+				printf("%11.7f %11f\n",
+						x,
+						poly_eval(poly_coefs, poly_power, x));
+			}
+			break;		
 		}
 	}
 
