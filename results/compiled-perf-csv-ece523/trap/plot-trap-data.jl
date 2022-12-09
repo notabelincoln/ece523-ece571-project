2d0c858e8d6b4580c54d8f48e420f73615c4c4aa
Abe <abejordandx@gmail.com>
2022-12-09 12:43:11 -0500

    Added plotting scripts

---
+++ results/compiled-perf-csv-ece523/trap/plot-trap-data.jl
@@ -0,0 +1,21 @@
+#!/usr/bin/julia
+
+using Plots, CSV, DataFrames, Printf, StatsPlots
+
+legend_names = ["5.11" "16.16" "32.32" "Float" "Double"]
+y_labels = Dict("energy"=>"Energy (J)", "error"=>"Absolute Error",
+		"time"=>"Time (s)", "power"=>"Power (W)")
+
+for m in ["energy", "error", "time", "power"]
+	local df = DataFrame(CSV.File(@sprintf("trap-%s.csv", m)));
+	local y_max = 1.1 * maximum(Matrix(df[:,2:end]));
+
+	local plt = groupedbar(Matrix(df[:,2:end]),
+			       ylims = (0, y_max),
+			       xticks = (1:nrow(df), string.(df[:,1])),
+			       xlabel = "x Scaling Factor",
+			       ylabel = y_labels[m],
+			       label = legend_names,
+			       framestyle = :box);
+	savefig(plt, @sprintf("trap-%s.png", m));
+end
